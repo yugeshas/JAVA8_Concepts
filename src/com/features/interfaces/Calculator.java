f1b9bc8fb5cd28291de17d607f72141d4e14fa10
Yugesha Sapte <yugeshasapte.4@gmail.com>
2020-09-03 16:35:41 -0500

    Adding : lambda function for integer operations

---
+++ src/com/features/interfaces/Calculator.java
@@ -0,0 +1,7 @@
+package com.features.interfaces;
+
+public interface Calculator {
+	
+	public int operations(int a, int b); 
+
+}
---
+++ src/com/features/lambda/LambdaUsingInterface.java
@@ -0,0 +1,53 @@
+package com.features.lambda;
+import com.features.interfaces.Calculator;
+import com.features.interfaces.Print;
+
+public class LambdaUsingInterface{
+	
+	//Define all simple calculations 
+	Calculator addition = (a, b) -> a + b; 
+	
+	Calculator subtraction = (a, b) -> a - b; 
+	
+	static Calculator multiplication = (a, b) -> a * b; 
+	
+	Calculator division = (a, b) -> a/b;
+	
+	//Can use {} - {} are optional 
+	static Print printAns = (a) -> { System.out.println(a); }; 
+	
+	//write functions to access lambda methods. 
+	private int calculate(int a, int b, Calculator operation) {
+	      return operation.operations(a, b);
+    }
+	
+	private void print(Object a, Print printAns){
+		printAns.print(a);
+	}
+	
+	public static void main(String args[]){
+		
+		// To access methods first create object for current class		
+		LambdaUsingInterface lui = new LambdaUsingInterface(); 
+		
+		
+		//initialize data 
+		int a = 6, b = 2; 
+		
+		//Lets perform all operations for a and b. print ans.
+		System.out.println("Access lambda functions through class methods ");
+		int add = lui.calculate(a, b, lui.addition);		
+		int sub = lui.calculate(add,  b,  lui.subtraction);
+		lui.print(add, printAns);
+		lui.print(sub, printAns);
+		
+		//using static reference 
+		System.out.println("Access lambda functions by declaring it as static variables");
+		lui.print(lui.calculate(add, sub, multiplication), printAns); 
+		lui.print(lui.calculate(add, sub, lui.division), printAns); 
+			
+		
+	
+	}
+
+}
